ae9a4dbde8178e771c712dd6059bb044ddbd5224
Aaron Liu <shurui91@gmail.com>
2023-10-03 16:37:27 -0700

    lecture 11

--- src/App.js
+++ src/App.js
@@ -2,24 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const title = 'Blog Post';
+	const body = 'This is a Blog Post';
+	const comments = [
+		{ id: 1, text: 'Comment 1' },
+		{ id: 2, text: 'Comment 2' },
+		{ id: 3, text: 'Comment 3' },
+	];
+
+	const loading = false;
+	const showComments = true;
+	if (loading) return <h1>Loading...</h1>;
+	const commentBlock = (
+		<div className='comments'>
+			<h3>Comments ({comments.length})</h3>
+			<ul>
+				{comments.map((comment, index) => (
+					<li key={index}>{comment.text}</li>
+				))}
+			</ul>
+		</div>
+	);
+
+	return (
+		<div className='container'>
+			<h1>{title.toUpperCase()}</h1>
+			<p>{body}</p>
+			{showComments && commentBlock}
+		</div>
+	);
 }
 
 export default App;
