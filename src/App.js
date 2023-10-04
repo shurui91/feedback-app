177e21b60a3f52a6b5bfdaac2d7d0f837f7777eb
Aaron Liu <shurui91@gmail.com>
2023-10-03 17:13:34 -0700

    lecture 13, css file added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-	const title = 'Blog Post';
-	const body = 'This is a Blog Post';
-	const comments = [
-		{ id: 1, text: 'Comment 1' },
-		{ id: 2, text: 'Comment 2' },
-		{ id: 3, text: 'Comment 3' },
-	];
-
-	const loading = false;
-	const showComments = true;
-	if (loading) return <h1>Loading...</h1>;
-	const commentBlock = (
-		<div className='comments'>
-			<h3>Comments ({comments.length})</h3>
-			<ul>
-				{comments.map((comment, index) => (
-					<li key={index}>{comment.text}</li>
-				))}
-			</ul>
-		</div>
-	);
-
 	return (
-		<div className='container'>
-			<h1>{title.toUpperCase()}</h1>
-			<p>{body}</p>
-			{showComments && commentBlock}
-		</div>
-	);
+		<>
+			<Header text='Hello World' />
+			<div className="container">
+				<h1>My App</h1>
+			</div>
+		</>
+	)
 }
 
 export default App;
+;
