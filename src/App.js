2ba4c52939f2f6e0f155c3ebc837effa1542d92b
Aaron Liu <shurui91@gmail.com>
2023-10-05 14:35:02 -0700

    react router part updated, lecture 26

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { useState } from 'react';
 import Header from './components/Header';
@@ -6,6 +7,7 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import FeedbackData from './data/FeedbackData';
+import AboutPage from './pages/AboutPage';
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
@@ -22,17 +24,25 @@ function App() {
 	};
 
 	return (
-		<>
+		<Router>
 			<Header />
 			<div className='container'>
-				<FeedbackForm handleAdd={addFeedback} />
-				<FeedbackStats feedback={feedback} />
-				<FeedbackList
-					feedback={feedback}
-					handleDelete={deleteFeedback}
-				/>
+				<Routes>
+					<Route exact path='/' element={
+						<>
+							<FeedbackForm handleAdd={addFeedback} />
+							<FeedbackStats feedback={feedback} />
+							<FeedbackList
+								feedback={feedback}
+								handleDelete={deleteFeedback}
+							/>
+						</>
+					}>
+					</Route>
+					<Route path='/about' element={<AboutPage />} />
+				</Routes>
 			</div>
-		</>
+		</Router>
 	);
 }
 
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,7 @@
+import Card from '../components/shared/Card';
+
+function AboutPage() {
+	return <Card>About Page</Card>;
+}
+
+export default AboutPage;
